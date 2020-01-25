b5272a5cf1e97416718a76866f6461b5ee365f01
syki66 <66syki@gmail.com>
2020-01-25 21:39:49 +0900

    setState changes

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component{
+  state = {
+    count: 0
+  }
+
+  numberUp = () => {
+    for(let i=0; i<100; i++){
+      this.setState((prevState) => ({ count: prevState.count + 1}));
+    }
+  }
+
+
+  componentDidMount() {
+      this.numberUp();    
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.count}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
